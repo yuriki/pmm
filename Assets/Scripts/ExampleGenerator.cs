024ca5151cf8ecd79ec1680914defd5805eca9e7
yuriki <3dyuriki@gmail.com>
2018-05-29 20:27:38 +0300

    Finished generation of InColumn example

--- Assets/Scripts/ExampleGenerator.cs
+++ Assets/Scripts/ExampleGenerator.cs
@@ -8,7 +8,7 @@ public class ExampleGenerator : MonoBehaviour
 	[Header("States")]
 	public StateData exampleSwitch;
 	public NonRangedStateData correctAnswer;
-	public StateData digitsNumber;
+	public StateData maxDigitsInUserInput;
 
 	[Header("Toggles")]
 	public ExampleTogglesData toggles5;
@@ -31,20 +31,12 @@ public class ExampleGenerator : MonoBehaviour
 	#endregion
 
 	#region Private_Variables
-	int[] tmpArray;
 
 	int generated1; 
 	int tmpGen1;
 
 	int generated2; 
 	int tmpGen2;
-
-	int randomToggle;
-	int signInt;
-	string signStr;
-
-	int minRandomValue;
-	int maxRandomValue;
 
 	// counters to help get rid of infinite while-loop and to make some values more rare
 	int i;
@@ -57,30 +49,31 @@ public class ExampleGenerator : MonoBehaviour
 	public void NewExample ()
 	{
 		//set limit of digits user can input (print on screen)
-		digitsNumber.Value = 3;
+		maxDigitsInUserInput.Value = 3;
 
 		inputResultText.text = "";
 		questionMark.GetComponent<Text>().text = "?";
 
 		if (exampleSwitch.Value == 0)								//up to ten example 9-1=?, 9+1=?, 9-?=3
 		{
+			int maxNum;
 			//if toggle "up to 10" is active
 			if (toggles10.toggles[3]) 
 			{
-				maxRandomValue = 10;
+				maxNum = 10;
 			}
 			else
 			{
-				maxRandomValue = 5;
+				maxNum = 5;
 			}
 			//if toggle "?" (example with unknown 9-?=3) is active
 			if (toggles10.toggles[4]) 
 			{
-				GenerateCountTo10Example(maxRandomValue, true);
+				GenerateCountTo10Example(maxNum, true);
 			}
 			else
 			{
-				GenerateCountTo10Example(maxRandomValue, false);
+				GenerateCountTo10Example(maxNum, false);
 			}
 		}
 		else if (exampleSwitch.Value == 1)							//multiplication table example 3x4=?
@@ -96,19 +89,18 @@ public class ExampleGenerator : MonoBehaviour
 		}
 		else if (exampleSwitch.Value == 2)							//in column example
 		{
+			int mult;
 			//if toggle "100" is active
 			if (togglesColumn.toggles[3])
 			{
-				maxRandomValue = 1000;
-				minRandomValue = 101;
+				mult = 10;
 			}
 			else
 			{
-				maxRandomValue = 100;
-				minRandomValue = 12;
+				mult = 1;
 			}
 
-			GenerateColumnExample();
+			GenerateColumnExample(mult);
 			ArrangeColumnExample();
 		}
 
@@ -122,6 +114,8 @@ public class ExampleGenerator : MonoBehaviour
 
 	void GenerateCountTo10Example(int maxValue, bool isHardExample)
 	{
+		int signInt;
+		string signStr;
 		// Choosing sign based on active toggle (among SecondLvlToggles)
 		if (ChooseToggleWithValueRandomly(toggles10) == 1)
 		{
@@ -134,7 +128,8 @@ public class ExampleGenerator : MonoBehaviour
 			signStr = "+";
 		}
 
-		tmpArray = GenerateRandomPair(maxValue);
+		//I'm using array because of .NET 3.5 (still no tuple support)
+		int[] tmpArray = GenerateRandomPairWithCondition(maxValue, signInt);
 		generated1 = tmpArray[0];
 		generated2 = tmpArray[1];
 
@@ -143,12 +138,12 @@ public class ExampleGenerator : MonoBehaviour
 		{
 			if (UnityEngine.Random.Range(0, 5) == 0) //1 of 5 examples will be normal (without unknown)
 			{
-				PrintNormalExample();
+				PrintNormalExample(signInt, signStr);
 			}
 			else
 			{
 				//IMPORTANT! changing this value I'm doing imposible for user to input more than one digit in answer
-				digitsNumber.Value = 1;
+				maxDigitsInUserInput.Value = 1;
 
 				correctAnswer.Value = generated1 + signInt * generated2;
 
@@ -170,33 +165,43 @@ public class ExampleGenerator : MonoBehaviour
 		}
 		else //for other type of examples - "1+3=?" and "6-4=?"
 		{
-			PrintNormalExample();
+			PrintNormalExample(signInt, signStr);
 		}
 	}
 
 
-	void PrintNormalExample()
+	void PrintNormalExample(int signI, string signS)
 	{
 		MoveUserInputMarker(userTopRight);
 		MoveRightSideOfRectTransform(mathExamp.gameObject.GetComponent<RectTransform>(), 0);
 
-		mathExamp.text = generated1 + signStr + generated2 + "=";
-		correctAnswer.Value = generated1 + signInt * generated2;
+		mathExamp.text = generated1 + signS + generated2 + "=";
+		correctAnswer.Value = generated1 + signI * generated2;
 	}
 
-
-	int[] GenerateRandomPair(int maxInt)
+	/// <summary>
+	/// Randomly generates two numbers (INT) which meet condition: (gen1 +/- gen2) must be bigger than 0 but smaller than maxInt.
+	/// There is built in protection against to often generating zero value.
+	/// </summary>
+	/// <param name="maxInt">Maximum number that could be generated</param>
+	/// <param name="sign">Sign "+" or "-"</param>
+	/// <returns></returns>
+	int[] GenerateRandomPairWithCondition(int maxInt, int sign)
 	{
 		int gen1, gen2;
-		int correction = 1; //1 - if I want to max generated number = 10 (or 5): 10+0=?, 10-3=?. Else (max generated value = 9) correction = 0
+
+		//Set "correction" value to "1" if you want max generated number = 10 or 5 (to get examples like 10+0=?, 10-3=?).
+		//Set it to "0" if you want max generated value = 9 or 4.
+		int correction = 1;
 
 		gen1 = UnityEngine.Random.Range(0, maxInt + correction);
-		i = 1;
+		i = 0;
 		// I'm making next generated value different from previous one (different from tmpGenerated)
 		while (tmpGen1 == gen1)
 		{
 			gen1 = UnityEngine.Random.Range(0, maxInt + correction);
 
+			//TODO redo this protection againts zero value because it is inside while-loop that is not always firing
 			//protection against too often zero value
 			if (gen1 == 0)
 			{
@@ -221,10 +226,10 @@ public class ExampleGenerator : MonoBehaviour
 		j = 1;
 		//IMPORTANT I can use expression "== 0" here only if I regenerate variable "generated". Oterwise I'll have infinite loop in some cases
 		//For example, when "gen1 = 9" and previous "gen2 = 1", I can't generate 1 again and I have infinite loop
-		while ((gen1 + signInt * gen2) <= 0 || (gen1 + signInt * gen2) > maxInt)
+		while ((gen1 + sign * gen2) <= 0 || (gen1 + sign * gen2) > maxInt)
 		{
 			//protection against too often zero result (zero result could be only if we have two the same values 0+0=0, 3-3=0, 8-8=0)
-			if ((gen1 + signInt * gen2) == 0)
+			if ((gen1 + sign * gen2) == 0)
 			{
 				if (j > 3) //3 tries
 				{
@@ -397,8 +402,11 @@ public class ExampleGenerator : MonoBehaviour
 	}
 
 
-	void GenerateColumnExample()
+	void GenerateColumnExample(int multiplier)
 	{
+		int signInt;
+		string signStr;
+
 		if (ChooseToggleWithValueRandomly(togglesColumn) == 1)
 		{
 			signInt = -1;
@@ -410,75 +418,117 @@ public class ExampleGenerator : MonoBehaviour
 			signStr = "+";
 		}
 
-		generated1 = UnityEngine.Random.Range(1, 20);
-		generated2 = UnityEngine.Random.Range(1, 19);
-		//while (minRandomValue > generated1 + generated2 || maxRandomValue < generated1 + generated2)
-		//{
-		//	if (generated2 == 550)
-		//	{
-		//		generated2 = 1;
-		//	}
-		//	else
-		//	{
-		//		generated2++;
-		//	}
+		// I'm making next generated value different from previous one (tmpGenerated)
+		while (tmpGen1 == generated1)
+			generated1 = UnityEngine.Random.Range(1*multiplier*multiplier, 60*multiplier);
 
-		//	while (tmpGen1 == generated1)
-		//		generated1 = UnityEngine.Random.Range(1, 50);
+		while (tmpGen2 == generated2)
+			generated2 = UnityEngine.Random.Range(1, 40*multiplier);
 
-		//	while (tmpGen2 == generated2)
-		//		generated2 = UnityEngine.Random.Range(1, 550);
-		//}
+		int i = 0;
+		int j = 0;
+		int k = 0;
+		//TODO make protection against infinite loop when toggle ">100" is active
+		while (11*multiplier > generated1 + signInt * generated2 || 99*multiplier < generated1 + signInt * generated2)
+		{
+			//first I'm trying 10x times to regenerate generated2
+			if (i != 10)
+			{
+				i++;
+				generated2 = UnityEngine.Random.Range(1, 40 * multiplier);
+			}
+			else //if I'm not successed with 10x tries, I'm trying to look through all values starting from 1
+			{
+				if (generated2 == 39 * multiplier)
+				{
+					//protection againts too often "1" value for generated2 (I'm trying 10 times before accept value "1")
+					if (j != 10)
+					{
+						j++;
+						generated2 = UnityEngine.Random.Range(2, 40 * multiplier); //min value is "2" to get rid of "1"s
+						// I need to regenerate generated1 if sing is "-" to get rid of infinite while-loop
+						if (signInt == -1)
+						{
+							//min value is 20*multiplier to get rid of small numbers (1-40=? etc)
+							generated1 = UnityEngine.Random.Range(20 * multiplier, 60 * multiplier);
+						}
+					}
+					else
+					{
+						generated2 = 1;
+						// I need to regenerate generated1 if sing is "-" to get rid of infinite while-loop
+						if (signInt == -1)
+						{
+							//min value is 20*multiplier to get rid of small numbers (1-40=? etc)
+							generated1 = UnityEngine.Random.Range(20*multiplier, 60 * multiplier);
+						}
+					}
+				}
+				else
+				{
+					generated2++;
+				}
+			}
+
+			while (tmpGen1 == generated1)
+				generated1 = UnityEngine.Random.Range(1, 60*multiplier);
+
+			//final STUPID protection
+			//TODO get rid of this
+			if (k != 100)
+			{
+				k++;
+			}
+			else
+			{
+				generated1 = UnityEngine.Random.Range(17, 37);
+				generated2 = UnityEngine.Random.Range(8, 16);
+				break;
+			}
+		}
+
 		tmpGen1 = generated1;
 		tmpGen2 = generated2;
 
-		// I'm making next generated value different from previous one (tmpGenerated)
-		//while (tmpGen1 == generated1)
-		//	generated1 = UnityEngine.Random.Range(10, 801);
-		//tmpGen1 = generated1;
-
-		//while (tmpGen2 == generated2)
-		//	generated2 = UnityEngine.Random.Range(1, 200);
-		//tmpGen2 = generated2;
-
-		mathExamp.text = generated1.ToString() + "\n" + "+        \n" + generated2.ToString() + "\n" + "-------";
-		correctAnswer.Value = generated1 + generated2;
+		mathExamp.text = generated1 + "\n" + signStr + "        \n" + generated2 + "\n" + "-------";
+		correctAnswer.Value = generated1 + signInt * generated2;
 	}
 
 
 	int ChooseToggleWithValueRandomly(ExampleTogglesData toglesData)
 	{
+		int minToggle, maxToggle, randomToggle;
 		//choosing MIN and Max value for Random function (because I need to choose only numbers when this is MultTable example)
 		if (exampleSwitch.Value == 1) //if MultTable
 		{
-			minRandomValue = 2;
-			maxRandomValue = 9;
+			minToggle = 2;
+			maxToggle = 9;
 		}
 		else
 		{
-			minRandomValue = 0;
-			maxRandomValue = 1;
+			minToggle = 0;
+			maxToggle = 1;
 		}
 
-		randomToggle = UnityEngine.Random.Range(minRandomValue, maxRandomValue + 1);
+		randomToggle = UnityEngine.Random.Range(minToggle, maxToggle + 1);
 		i = 1;
 		//checking if randomly picked toggle is ON
 		while (!toglesData.toggles[randomToggle])
 		{
-			if (randomToggle != maxRandomValue)
+			if (randomToggle != maxToggle)
 			{
 				randomToggle++;
 			}
 			else
 			{
-				randomToggle = minRandomValue;
+				randomToggle = minToggle;
 			}
 
 			//Foolproof. In case NO toggles were selected, the default value will be chosen: "+" (plus) for all type examples or "2" (two) for MultTable
 			i++;
 			if (i > 10) //10 because I want loop goes through twice before exit with default value
 			{
-				randomToggle = minRandomValue;
+				randomToggle = minToggle;
 				break;
 			}
 
